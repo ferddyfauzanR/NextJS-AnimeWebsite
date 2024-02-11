2801c648257b50a86a42208aefce1f7ad001f5d8
Ferddy Fauzan <ferddyfauzan@gmail.com>
2024-02-11 20:10:13 +0700

    Setup Folder , Handle Limit Card Load In HomePage , Styling Card

--- next-anime/src/app/page.js
+++ next-anime/src/app/page.js
@@ -1,17 +1,27 @@
-import AnimeList from "./components/AnimeList"
+import Link from "next/link"
+import AnimeList from "@/components/AnimeList"
 
 const Home = async() => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=10`)
   const anime = await response.json()
 
   anime.data
   
   return (
     <div>
-      <h1>Populer anime</h1>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="p-4 flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Populer anime</h1>
+        <Link href='/populer' className="md:text-md text-sm underline hover:text-indigo-500 transition-all">Lihat Semua</Link>
+      </div>
+      
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
       {anime.data.map(data => {
-        return <AnimeList title={data.title} images={data.images.webp.image_url} />
+        return (
+          <div key={data.mal_id} className="shadow-xl">
+          <AnimeList id={data.mal_id} title={data.title} images={data.images.webp.image_url} />
+          </div>
+        )
+       
       })}
       </div>
       
---
+++ next-anime/src/app/populer/page.js
@@ -0,0 +1,9 @@
+const page = () => {
+    return(
+        <>TEST
+        </>
+    )
+}
+
+
+export default page
