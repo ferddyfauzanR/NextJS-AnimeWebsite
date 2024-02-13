1df406a1fe8c82ced9aa22fd09d80e7a8c61f7f1
Ferddy Fauzan <ferddyfauzan@gmail.com>
2024-02-13 10:25:43 +0700

    styling navbar

--- next-anime/src/components/Navbar/index.js
+++ next-anime/src/components/Navbar/index.js
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 const Navbar = () =>{
     return(
-        <header className="bg-indigo-500">
+        <header className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="flex md:flex-row flex-col justify-between p-4 gap-2">
-                <Link href="/"className="font-bold text-white text-2xl">WebsiteAnimeBandung</Link>
-                <input placeholder="Cari Anime..." className=""/>
+                <Link href="/"className="font-bold text-white text-2xl hover:bg-white hover:rounded-xl hover:text-black p-2">WebsiteAnimeBandung</Link>
+                <input placeholder=" Cari Anime..." className="rounded-xl"/>
             </div>
         </header>
     )
